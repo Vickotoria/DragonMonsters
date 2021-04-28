efd7a04feb4a7dee2d39a93ccbf129c608fdbb31
Vickotoria <victoria.sparrman@outlook.com>
2021-04-28 21:28:20 +0200

    Setup test.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * The main class for the Dragon Monsters project.
+ */
+public class Main {
+    //
+
+    public static void main(String[] args) {
+        System.out.println("hej");
+    }
+}
