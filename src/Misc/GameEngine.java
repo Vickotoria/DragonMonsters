d0a5fb75b63cc5ecb83bc745e06ab87bdb03c9ee
Vickotoria <victoria.sparrman@outlook.com>
2021-04-28 22:08:37 +0200

    Added in the GameEngine class.

--- src/Misc/GameEngine.java
+++ src/Misc/GameEngine.java
@@ -1,5 +1,6 @@
 package Misc;
 
+import Monsters.Monster;
 import Trainers.EnemyTrainer;
 import Trainers.Player;
 
@@ -25,15 +26,20 @@ public class GameEngine {
         //monsters
 
         //player
-        player = new Player(askPlayerName(), null, null);
+        player = new Player(askPlayerName(), null, null); //todo fix
         System.out.println(player.getName()); //todo remove
 
         //enemies
-
+        enemyTrainers = new ArrayList<>(1);
+        loadEnemies();
     }
 
     private String askPlayerName(){
         System.out.println("What is your name?");
         return scanner.nextLine();
+    }
+
+    private void loadEnemies() {
+        //
     }
 }
