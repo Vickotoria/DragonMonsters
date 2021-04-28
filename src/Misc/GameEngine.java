6ffc656e85bf7e21b5a6ffd5e565eb10b9699b61
Vickotoria <victoria.sparrman@outlook.com>
2021-04-28 22:05:56 +0200

    Added the beginnings of the GameEngine class.

--- src/Misc/GameEngine.java
+++ src/Misc/GameEngine.java
@@ -1,4 +1,39 @@
 package Misc;
 
+import Trainers.EnemyTrainer;
+import Trainers.Player;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GameEngine {
+    //private final int maxNumMon = 5;
+
+    private Scanner scanner;
+
+    private Player player;
+    private ArrayList<EnemyTrainer> enemyTrainers;
+
+    public GameEngine(){
+        setupGame();
+    }
+
+    private void setupGame(){
+        //basic setup
+        scanner = new Scanner(System.in);
+
+        //monsters
+
+        //player
+        player = new Player(askPlayerName(), null, null);
+        System.out.println(player.getName()); //todo remove
+
+        //enemies
+
+    }
+
+    private String askPlayerName(){
+        System.out.println("What is your name?");
+        return scanner.nextLine();
+    }
 }
